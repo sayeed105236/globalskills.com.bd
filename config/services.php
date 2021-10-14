8e58fe279dc4407af1ec997ea1495c0dd39af548
Md Abu Sayeed <abusayeed33343536@gmail.com>
2021-10-14 15:54:56 +0600

    service update

--- config/services.php
+++ config/services.php
@@ -32,13 +32,13 @@ return [
     'google' => [
       'client_id' => env('GOOGLE_CLIENT_ID'),
       'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-     'redirect' => 'https://globalskills.com.bd/login/google/callback',
+     'redirect' => 'https://www.globalskills.com.bd/login/google/callback',
   ],
 
   'facebook' => [
       'client_id' => env('FACEBOOK_CLIENT_ID'),
       'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-      'redirect' => 'https://globalskills.com.bd/login/facebook/callback',
+      'redirect' => 'https://www.globalskills.com.bd/login/facebook/callback',
   ],
 
 ];
